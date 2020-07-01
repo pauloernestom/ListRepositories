e2a7753981c45e508ba0683c487200e960d28714
paulo <pauloernestom@gmail.com>
2020-07-01 16:39:58 -0300

    Mensagem de erro quando usuário não encontrado

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,30 +4,38 @@ import * as S from './styled';
 import { useHistory } from 'react-router-dom';
 
 function App(props) {
-  const [ usuario, setUsuario] = useState('')
-  // [usuario, setUsuario]
   const history = useHistory();
+  const [ usuario, setUsuario] = useState('');
+  const [erro, setErro ] = useState(false);
+  
 
   function handlePesquisa(){
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
-      const repositories = response.data;
-      const repositoriesName = [];
-      repositories.map((repository) => {
-        repositoriesName.push(repository.name)
+    axios.get(`https://api.github.com/users/${usuario}/repos`)
+      .then(response => {
+        const repositories = response.data;
+        const repositoriesName = [];
+        repositories.map((repository) => {
+          repositoriesName.push(repository.name)
+        })
+        localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+        setErro(false);
+        history.push('./repositories');
       })
-      localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
-      history.push('./repositories');
-    
-    });
+      .catch(err => {
+        setErro(true)
+      });
     
   }
   return (
-    <S.Container>
+    <S.HomeContainer>
       {/* <p>{ usuario }</p> */}
       {/* <h1>{props.title} {props.user}</h1> */}
-      <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
-      <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
-    </S.Container>
+      <S.Content>
+        <S.Input className="usuarioInput" placeholder="Usuário" value={usuario} onChange={e => setUsuario(e.target.value)}/>
+        <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+      </S.Content>
+      { erro ? <S.ErrorMsg>Ocorreu um erro. Tente novamente.</S.ErrorMsg> : '' }
+    </S.HomeContainer>
   );
 }
 
--- src/pages/Repositories/styled.js
+++ src/pages/Repositories/styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
     width: 100%;
@@ -22,4 +23,14 @@ export const ListItem = styled.li`
     background: #000;
     color: #fff;
     padding: .5rem;
+`;
+
+export const LinkHome = styled(Link)`
+    display: block;
+    width: 4rem;
+    text-align: center;
+    margin: 2rem auto;
+    background-color: #000;
+    color: #fff;
+    text-decoration: none;
 `;
